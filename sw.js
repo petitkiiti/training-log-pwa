8e326d907619c5aab6235ec0cb94d7af717f438a
Kiiti <petitkiiti@gmail.com>
2026-09-19 19:03:53 +0900

    Update to v0.10.10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='training-log-v0.10.4';
+const CACHE='training-log-v0.10.10';
 const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install', event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))); });
 self.addEventListener('activate', event => { event.waitUntil((async () => { const keys=await caches.keys(); await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))); await self.clients.claim(); })()); });
